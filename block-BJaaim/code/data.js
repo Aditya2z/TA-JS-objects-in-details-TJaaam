923e8d77bfe0c1b3ebcc465d2bd8e0b54e3bcfd5
Aditya Dhanraj <pandeydhanrajaditya@gmail.com>
2023-04-19 16:53:02 +0530

    Quiz App Complete

---
+++ block-BJaaim/code/data.js
@@ -0,0 +1,87 @@
+const questionsData = [  
+  {
+    title: "What is the capital city of France?",
+    options: ["London", "Paris", "Madrid"],
+    correctAnswerIndex: 1
+  },
+  {
+    title: "Who directed the movie 'Jaws'?",
+    options: ["Steven Spielberg", "Quentin Tarantino", "Alfred Hitchcock"],
+    correctAnswerIndex: 0
+  },
+  {
+    title: "Which continent is Egypt located in?",
+    options: ["Africa", "Asia", "Europe"],
+    correctAnswerIndex: 0
+  },
+  {
+    title: "Who is the main protagonist of the 'Lord of the Rings' trilogy?",
+    options: ["Aragorn", "Gandalf", "Frodo"],
+    correctAnswerIndex: 2
+  },
+  {
+    title: "Which of the following is a programming language?",
+    options: ["HTML", "CSS", "Java"],
+    correctAnswerIndex: 2
+  },
+  {
+    title: "What is the name of the world's largest desert?",
+    options: ["Sahara", "Gobi", "Antarctica"],
+    correctAnswerIndex: 0
+  },
+  {
+    title: "Which of these colors is not in the rainbow?",
+    options: ["Orange", "Red", "Pink"],
+    correctAnswerIndex: 2
+  },
+  {
+    title: "Who discovered the law of gravity?",
+    options: ["Isaac Newton", "Albert Einstein", "Galileo Galilei"],
+    correctAnswerIndex: 0
+  },
+  {
+    title: "Which planet in our solar system is known as the 'Red Planet'?",
+    options: ["Jupiter", "Mars", "Venus"],
+    correctAnswerIndex: 1
+  },
+  {
+    title: "What is the name of the tower that houses Big Ben?",
+    options: ["The Eiffel Tower", "The Empire State Building", "The Clock Tower"],
+    correctAnswerIndex: 2
+  },
+  {
+    title: "What is the highest mountain in the world?",
+    options: ["Mount Kilimanjaro", "Mount Everest", "Mount Fuji"],
+    correctAnswerIndex: 1
+  },
+  {
+    title: "Who painted 'The Starry Night'?",
+    options: ["Claude Monet", "Vincent van Gogh", "Pablo Picasso"],
+    correctAnswerIndex: 1
+  },
+  {
+    title: "What is the name of the longest river in Africa?",
+    options: ["Nile", "Amazon", "Yangtze"],
+    correctAnswerIndex: 0
+  },
+  {
+    title: "What is the name of the first man to walk on the moon?",
+    options: ["Neil Armstrong", "Buzz Aldrin", "Michael Collins"],
+    correctAnswerIndex: 0
+  },
+  {
+    title: "Which of the following is a programming paradigm?",
+    options: ["Object-Oriented", "Functional", "Imperative"],
+    correctAnswerIndex: 1
+  },
+  {
+    title: "What is the name of the Greek goddess of wisdom?",
+    options: ["Aphrodite", "Athena", "Artemis"],
+    correctAnswerIndex: 1
+  },
+  {
+    title: "Who wrote 'Pride and Prejudice'?",
+    options: ["Emily Bronte", "Charlotte Bronte", "Jane Austen"],
+    correctAnswerIndex: 2
+  },
+]
